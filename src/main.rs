a7c8a220ef1000b6f6e27840984d4b425f27f00f
maer09 <maer09@users.noreply.github.com>
2024-04-20 20:09:32 +0800

    the first step of a cli.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,16 @@
 // rcli csv -i input.csv -o output.json --header -d ','
 
 use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
 
-#[derive(Debug, Parser)]
-#[clap(name = "rcli", version, author, about, long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?
+        }
+    }
+
+    Ok(())
 }
 
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")]
-    output: String,    
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
-
-fn main() {
-    let opt = Opts::parse();
-    println!("{:?}", opt);
-}
