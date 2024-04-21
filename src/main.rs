407c195a891a779fba6af6bc9f3fc0e6b3d13900
maer09 <maer09@users.noreply.github.com>
2024-04-21 20:47:03 +0800

    初步实现生成随机密码

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // rcli csv -i input.csv -o output.json --header -d ','
 
 use clap::Parser;
-use rcli::{process_csv, Opts, SubCommand};
+use rcli::{process_csv, process_genpass, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
@@ -13,6 +13,9 @@ fn main() -> anyhow::Result<()> {
                 "output.json".into()
             };
             process_csv(&opts.input, output, opts.format)?;
+        }
+        SubCommand::GenPass(opts) => {
+            process_genpass(opts.length, opts.uppercase, opts.lowercase, opts.number, opts.symbol)?
         }
     }
 
