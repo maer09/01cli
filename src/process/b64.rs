51e1e6d391cce762670a8f2d1073060a4e3ebdff
maer09 <maer09@users.noreply.github.com>
2024-04-22 22:50:32 +0800

    完成b64的编码解码

--- src/process/b64.rs
+++ src/process/b64.rs
@@ -4,21 +4,16 @@ use base64::{
     Engine as _,
 };
 use std::{fs::File, io::Read};
+use anyhow::Result;
 
 pub fn process_encode(input: &str, format: Base64Format) -> anyhow::Result<()> {
-    // 用Box和syn还有Trait消除多种类型
-    let mut reader: Box<dyn Read> = if input == "-" {
-        // windows命令行下下按ctrl+z输入EOF才能终止输入
-        Box::new(std::io::stdin())
-    } else {
-        Box::new(File::open(input)?)
-    };
-
+    let mut reader = get_reader(input)?;
     let mut buf = Vec::new();
     reader.read_to_end(&mut buf)?;
 
     let encoded = match format {
         Base64Format::Standard => STANDARD.encode(&buf),
+
         Base64Format::UrlSafe => URL_SAFE_NO_PAD.encode(&buf),
     };
     println!("{}", encoded);
@@ -26,13 +21,20 @@ pub fn process_encode(input: &str, format: Base64Format) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn process_decode(input: &str, format: Base64Format) -> anyhow::Result<()> {
+fn get_reader(input: &str) -> Result<Box<dyn Read>> {
     // 用Box和syn还有Trait消除多种类型
-    let mut reader: Box<dyn Read> = if input == "-" {
+    let reader: Box<dyn Read> = if input == "-" {
+        // windows命令行下下按ctrl+z输入EOF才能终止输入
         Box::new(std::io::stdin())
     } else {
         Box::new(File::open(input)?)
     };
+
+    Ok(reader)
+}
+
+pub fn process_decode(input: &str, format: Base64Format) -> anyhow::Result<()> {
+    let mut reader = get_reader(input)?;
 
     let mut buf = String::new();
     reader.read_to_string(&mut buf)?;
@@ -58,14 +60,14 @@ mod tests {
     #[test]
     fn test_process_encode() {
         let input = "Cargo.toml";
-        let format = Base64Format::Standard;
+        let format = Base64Format::UrlSafe;
         assert!(process_encode(input, format).is_ok());
     }
 
     #[test]
     fn test_process_decode() {
-        let input = "Cargo.toml";
-        let format = Base64Format::Standard;
+        let input = "fixtures/output.b64";
+        let format = Base64Format::UrlSafe;
         assert!(process_decode(input, format).is_ok());
     }
 }
